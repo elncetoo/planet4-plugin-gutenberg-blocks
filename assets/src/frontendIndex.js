764f64eb97ffb2716989166771865a73fd109b0c
Florent Hernandez <florent.hernandez@greenpeace.org>
2020-07-28 11:34:18 +0200

    PLANET-4808: Convert Split Two Columns to WYSIWYG
    Ref: https://jira.greenpeace.org/browse/PLANET-4808
    Also solves:
    - https://jira.greenpeace.org/browse/PLANET-4317
    - https://jira.greenpeace.org/browse/PLANET-5205
    
    Conversion of Two Split Columns block to WYSIWYG.
    
    Addition of 2 js syntaxes, for easier handling of rest api results:
    - nullish coalescing operator `??` https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
    - optional chaining operator `?.` https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -1,11 +1,13 @@
 import { SpreadsheetFrontend } from './blocks/Spreadsheet/SpreadsheetFrontend';
 import { CounterFrontend } from './blocks/Counter/CounterFrontend';
 import { ArticlesFrontend } from './blocks/Articles/ArticlesFrontend';
+import { SplittwocolumnsFrontend } from "./blocks/Splittwocolumns/SplittwocolumnsFrontend";
 
 const COMPONENTS = {
   'planet4-blocks/spreadsheet': SpreadsheetFrontend,
   'planet4-blocks/counter': CounterFrontend,
   'planet4-blocks/articles': ArticlesFrontend,
+  'planet4-blocks/split-two-columns': SplittwocolumnsFrontend
 };
 
 document.querySelectorAll( `[data-render]` ).forEach(
